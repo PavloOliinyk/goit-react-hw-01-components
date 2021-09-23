b8072f9eb3d2dc2ad9e8ef1bdd5b2a4f73724b2f
Pavlo Oliinyk <oliinyk.pavlo@gmail.com>
2021-09-23 18:16:53 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import Profile from './components/profile/Profile';
-import Statistics from './components/statistics/Statistics';
-import FriendList from './components/friend-list/FriendList';
-import FriendListItem from './components/friend-list/FriendListItem';
-import TransactionHistory from './components/transaction-history/TransactionHistory';
-import userInfo from './user.json';
-import statisticalData from './statistical-data.json';
-import friends from './friends.json';
-import transactions from './transactions.json';
+import Profile from "./components/Profile";
+import Statistics from "./components/Statistics";
+import FriendList from "./components/FriendList";
+import FriendListItem from "./components/FriendListItem";
+import TransactionHistory from "./components/TransactionHistory";
+import Container from "./components/Container";
+import userInfo from "./user.json";
+import statisticalData from "./statistical-data.json";
+import friends from "./friends.json";
+import transactions from "./transactions.json";
 
 function App() {
   return (
-    <div className="container">
+    <Container>
       <Profile
         name={userInfo.name}
         tag={userInfo.tag}
@@ -20,7 +21,7 @@ function App() {
       />
       <Statistics stats={statisticalData} />
       <FriendList>
-        {friends.map(friend => (
+        {friends.map((friend) => (
           <FriendListItem
             key={friend.id}
             avatar={friend.avatar}
@@ -31,7 +32,7 @@ function App() {
         ))}
       </FriendList>
       <TransactionHistory items={transactions} />;
-    </div>
+    </Container>
   );
 }
 
---
+++ src/components/FriendListItem/FriendListItem.js
@@ -0,0 +1,22 @@
+import PropTypes from "prop-types";
+import s from "./FriendListItem.module.css";
+
+function FriendListItem({ avatar, name, isOnline }) {
+  const activeClass = isOnline ? s.active : s.offline;
+
+  return (
+    <li className={s.item}>
+      <span className={activeClass}></span>
+      <img className={s.avatar} src={avatar} alt={name} width="48" />
+      <p className={s.name}>{name}</p>
+    </li>
+  );
+}
+
+FriendListItem.propTypes = {
+  avatar: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  isOnline: PropTypes.bool
+};
+
+export default FriendListItem;
